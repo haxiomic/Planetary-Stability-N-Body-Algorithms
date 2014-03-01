ebe83019b14df95b75d01845aec12aa268f073cb
haxiomic <haxiomic@gmail.com>
2014-03-01 20:30:24 +0000

    Working trails

--- Source/Main.hx
+++ Source/Main.hx
@@ -16,23 +16,30 @@ class Main {
 		renderer = new NBodyRenderer(flash.Lib.current.stage);
 
 		//currently using length: AU 	time: days 		mass: kg
-		var rCV = .000001562; //radiusConversionFactor
-		addBodyFromDatum(SolarBodyData.sun, 40, 0xFFA21F);
-		addBodyFromDatum(SolarBodyData.mercury, SolarBodyData.mercury.radius*rCV, 0xFFE26E);
+		var rCV = .000001; //radiusConversionFactor
+		addBodyFromDatum(SolarBodyData.sun, 20, 0xFFA21F);
+		var merucy = addBodyFromDatum(SolarBodyData.mercury, SolarBodyData.mercury.radius*rCV, 0xFFE26E);
+		renderer.addTrail(merucy, 21, 1, 0xFFE26E, 4);
+		
 		addBodyFromDatum(SolarBodyData.venus, SolarBodyData.venus.radius*rCV, 0xB55D16);
-		addBodyFromDatum(SolarBodyData.earth, SolarBodyData.earth.radius*rCV, 0x13B3F2);
+		var earth = addBodyFromDatum(SolarBodyData.earth, SolarBodyData.earth.radius*rCV, 0x13B3F2);
+		renderer.addTrail(earth, 24, 1, 0x13B3F2, 15);
+
 		addBodyFromDatum(SolarBodyData.mars, SolarBodyData.mars.radius*rCV, 0xBB1111);
 
-		var stepTimer:haxe.Timer = new haxe.Timer(10);
-		stepTimer.run = function():Void{
-			simulator.step(0.1);
-		};
+		//var stepTimer:haxe.Timer = new haxe.Timer(10);
+		//stepTimer.run = function():Void{
+		//	simulator.step(1);
+		//};
+
+		renderer.beforeRender = function():Void{
+			simulator.step(1);
+		}
 	}
 
 	inline function addBodyFromDatum(bd:BodyDatum, displayRadius:Float = 10, displayColor:Int = 0xFF0000):Body{
 		var b = simulator.addBody(new Body(bd.position, bd.velocity, bd.mass));
 		renderer.addSphericalBody(b, displayRadius, displayColor);
-		//renderer.addTrail(b, 500, 3);
 		return b;
 	}
 
--- Source/renderer/NBodyRenderer.hx
+++ Source/renderer/NBodyRenderer.hx
@@ -3,6 +3,8 @@ package renderer;
 import away3d.cameras.Camera3D;
 import away3d.containers.View3D;
 import away3d.controllers.HoverController;
+import away3d.core.base.Geometry;
+import away3d.core.base.SubGeometry;
 import away3d.entities.Mesh;
 import away3d.entities.SegmentSet;
 import away3d.lights.DirectionalLight;
@@ -20,13 +22,13 @@ import flash.events.Event;
 import flash.events.MouseEvent;
 import flash.geom.Vector3D;
 
+import flash.Vector;
 import simulator.Body;
 import simulator.NBodySimulator;
 
 class NBodyRenderer {
 	static inline public var displayUnitsPerSimulationUnitLength = 300;
-	public var beforeDraw:Void->Void = null;
-	public var afterDraw:Void->Void = null;
+	public var beforeRender:Void->Void = null;
 
 	public var stage:Stage;
 
@@ -79,10 +81,10 @@ class NBodyRenderer {
 		return rb;
 	}
 
-	public function addTrail(body:Body, segmentCount:Int = 20, thickness:Float = 1, color:Int = 0xFFFFFF):Trail{
-		var tr:Trail = new Trail(body, segmentCount, thickness, color);
+	public function addTrail(body:Body, segmentCount:Int = 20, thickness:Float = 1, color:Int = 0xFFFFFF, updateTick:Int = 10):Trail{
+		var tr:Trail = new Trail(body, segmentCount, thickness, color, updateTick);
 
-		view.scene.addChild(tr.segSet);
+		view.scene.addChild(tr.mesh);
 		renderables.push(tr);
 		return tr;
 	}
@@ -127,7 +129,7 @@ class NBodyRenderer {
 		#else
 			//Setup plane
 			plane = new Mesh(new PlaneGeometry(1000,1000));
-			plane.material = new ColorMaterial(0x1C1D1F,0.1);
+			plane.material = new ColorMaterial(0x1C1D1F,0.05);
 			plane.material.lightPicker = lightPicker;
 			view.scene.addChild(plane);
 		#end
@@ -147,8 +149,7 @@ class NBodyRenderer {
 	}
 	
 	private function render(?e:Event){
-		if(beforeDraw != null)beforeDraw();
-
+		if(beforeRender!=null)beforeRender();
 		//Camera control
 		if (move) {
 		 	cameraController.panAngle = 0.3*(Math.isNaN(stage.mouseX)?0:stage.mouseX - lastMouseX) + lastPanAngle;
@@ -161,8 +162,6 @@ class NBodyRenderer {
 		}
 
 		view.render();
-
-		if(afterDraw != null)afterDraw();
 	}
 
 
@@ -250,8 +249,101 @@ class SphericalBody implements IRenderableBody{
 	}
 }
 
+class Trail implements IRenderableBody{
+	public var body:Body;
+	public var mesh:Mesh;
+
+	var thickness:Float;
+	var subGeoms:Array<SubGeometry>;
+	var segments:Array<Vector<Float>>;
+	var si:Int = 0;
+	var lastV3:Vector3D;
+	var updateTick:Int = 0;
+	var uti:Int = -1;
+
+	public function new(b:Body, segmentCount:Int = 20, thickness:Float = 1, color:Int = 0xFFFFFF, updateTick:Int){
+		this.body = b;
+		this.thickness = thickness;
+		this.updateTick = updateTick;
+		this.lastV3 = new Vector3D();
+		this.subGeoms = new Array<SubGeometry>();
+		this.segments = new Array<Vector<Float>>();
+
+		//set last position vector to current position
+		this.lastV3.x = NBR.sToR(this.body.x);
+		this.lastV3.y = NBR.sToR(this.body.y);
+		this.lastV3.z = NBR.sToR(this.body.z);
+
+		//mesh geometry
+		var geometry = new Geometry();
+
+		//initialize quad, for each segment
+		var subGeometry:SubGeometry, verticies:Vector<Float>, ai:Array<UInt>, indicies:Vector<UInt>;
+		for (i in 0...segmentCount) {
+			subGeometry = new SubGeometry();
+			verticies = Vector.ofArray
+				([0.0, 0.0, 0.0,
+	 			  0.0, 0.0, 0.0,
+	 			  0.0, 0.0, 0.0,
+	 			  0.0, 0.0, 0.0]);
+
+			ai = [0, 1, 2, 1, 2, 3];
+			indicies = Vector.ofArray(ai);
+
+			subGeometry.updateVertexData(verticies);
+			subGeometry.updateIndexData(indicies);
+			geometry.addSubGeometry(subGeometry);
+
+			subGeoms.push(subGeometry);
+			segments.push(verticies);
+		}
+
+		mesh = new Mesh(geometry, new ColorMaterial(color));
+	}
+
+	public inline function update(){
+		uti++;
+		if(uti%updateTick!=0){return;}
+		if(si>=segments.length)si=0;//wrap around
+		var bx = NBR.sToR(body.x);var by = NBR.sToR(body.y);var bz = NBR.sToR(body.z);
+
+		setPlaneBetweenVectors(segments[si], thickness,
+			lastV3.x, lastV3.y, lastV3.z, 
+			bx, by, bz
+		);
+
+		subGeoms[si].updateVertexData(segments[si]);
+
+		lastV3.x = bx;
+		lastV3.y = by;
+		lastV3.z = bz;
+		si++;
+	}
+
+	static inline function setPlaneBetweenVectors(v:Vector<Float>, thickness:Float,
+												  v0x:Float, v0y:Float, v0z:Float,
+												  v1x:Float, v1y:Float, v1z:Float){
+		var ht = thickness*.5;
+		var dx:Float = v1x-v0x;
+		var dy:Float = v1y-v0y;
+		var dz:Float = v1z-v0z;
+		var l:Float = Math.sqrt(dx*dx+dy*dy+dz*dz);
+		//normalize displacement vector
+		dx/=l;dy/=l;dz/=l;
+		//& multiply by half thickness for reciprocal coordinates of vertices's  
+		dx*=ht;dy*=ht;dz*=ht;
+		//x, y, z
+		//v0
+		v[0] =  -dz+v0x;v[1]  = v0y;v[2]  =  dx+v0z;//0
+		v[3] =   dz+v0x;v[4]  = v0y;v[5]  = -dx+v0z;//1
+		//v1
+		v[6] = 	-dz+v1x;v[7]  = v1y;v[8]  =  dx+v1z;//2
+		v[9] =   dz+v1x;v[10] = v1y;v[11] = -dx+v1z;//3
+	}
+}
+
 //#! currently Segments are not supported; needs rewrite.
-class Trail implements IUpdateable{
+/*class Trail implements IUpdateable{
 	public var body:Body;
 	public var segSet:SegmentSet;
 
@@ -299,4 +391,4 @@ class Trail implements IUpdateable{
 
 		si++;
 	}
-}
+}*/
