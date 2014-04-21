d1616444f97a3ac36f894d41a4ede63b379a5e63
Haxiomic <haxiomic@gmail.com>
2014-04-21 02:59:00 +0100

    Beginning transition to flat vectors for speed

--- Source/simulator/Hermite4thOrder.hx
+++ Source/simulator/Hermite4thOrder.hx
@@ -82,11 +82,11 @@ class Hermite4thOrder extends NBodySimulator{
 
 	@:noStack 
 	override function step(){
-		for(i in 0...bodyCount){
-			oldPosition[i].setProduct(position[i], 1);
-			oldVelocity[i].setProduct(velocity[i], 1);
-			oldAcceleration[i].setProduct(acceleration[i], 1);
-			oldJerk[i].setProduct(jerk[i], 1);
+		for(i in 0...bodyCount*3){
+			_f_oldPosition[i] = _f_position[i];
+			_f_oldVelocity[i] = _f_velocity[i];
+			_f_oldAcceleration[i] = _f_acceleration[i];
+			_f_oldJerk[i] = _f_jerk[i];
 		}
 
 		predict();
@@ -206,6 +206,7 @@ class Hermite4thOrder extends NBodySimulator{
 
 abstract FlatVec3Array(Vector<Float>){
 	static inline var VEC_SIZE:Int = 3;
+
 	public inline function new(count:Int){
 		this = new Vector<Float>(count*VEC_SIZE);
 	}
@@ -216,9 +217,35 @@ abstract FlatVec3Array(Vector<Float>){
 	@:extern public inline function set(index:Int, k:Int, value:Float):Float
 		return this[index*VEC_SIZE + k] = value;
 
+	@:extern public inline function getX(index:Int):Float
+		return this[index*VEC_SIZE];
+	@:extern public inline function getY(index:Int):Float
+		return this[index*VEC_SIZE+1];
+	@:extern public inline function getZ(index:Int):Float
+		return this[index*VEC_SIZE+2];
+	@:extern public inline function setX(index:Int, value:Float):Float
+		return this[index*VEC_SIZE] = value;	
+	@:extern public inline function setY(index:Int, value:Float):Float
+		return this[index*VEC_SIZE + 1] = value;	
+	@:extern public inline function setZ(index:Int, value:Float):Float
+		return this[index*VEC_SIZE + 2] = value;
+
 	@:extern public inline function setVec3(index:Int, v:Vec3):Void{
 		for (k in 0...VEC_SIZE)
 			this.set(index, v[k]);
+	}
+
+	//Vec3 Mirror
+	@:extern public inline function setFn(index:Int, fn:Int->Float){
+		setX(index, fn(0));
+		setY(index, fn(1));
+		setZ(index, fn(2));
+	}
+	
+	@:extern public inline function addFn(index:Int, fn:Int->Float){
+		setX(index, fn(0) + getX(index));
+		setY(index, fn(1) + getY(index));
+		setZ(index, fn(2) + getZ(index));
 	}
 
 	static public inline function fromArrayVec3(array:Array<Vec3>){
